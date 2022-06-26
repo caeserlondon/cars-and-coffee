e7e3e13e702ce9ee55c729a7ed1235424c9a1a6c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-26 19:56:31 +0100

    Adding the Router and the Show case component

--- client-side/components/Layout.js
+++ client-side/components/Layout.js
@@ -1,19 +1,26 @@
 import Head from 'next/head'
-import styles from '../styles/Layout.module.css'
+import { useRouter } from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, description, keywords, children }) {
+	const router = useRouter()
+
 	return (
 		<div>
 			<Head>
 				<title>{title}</title>
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
-				{/* <link rel={rel} href={href} /> */}
 			</Head>
+
 			<Header />
+
+			{router.pathname === '/' && <Showcase />}
 			<div className={styles.container}>{children}</div>
+
 			<Footer />
 		</div>
 	)
