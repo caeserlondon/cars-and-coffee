beed7dc7bbcd238bf008aea04366a1a2fe1f27ed
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-26 19:55:02 +0100

    Creating the Showcase component

---
+++ client-side/components/Showcase.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+	return (
+		<div className={styles.showcase}>
+			<h1>We Love Cars, We Love Coffee.</h1>
+		</div>
+	)
+}
