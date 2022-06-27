773cfb7023c7f14ccd2ccf527ab9c5091268a4ba
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-27 20:41:50 +0100

    Adding the events to the events page

--- client-side/pages/events/index.js
+++ client-side/pages/events/index.js
@@ -1,9 +1,39 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import Head from 'next/head'
+import { API_URL } from '@/config/index'
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+	// this will log in the client side
+	// console.log(events)
+
 	return (
-		<Layout title='Upcoming Events'>
-			<h1>My Events</h1>
+		<Layout>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<h1>Events</h1>
+
+			{events.length === 0 && <h2>No New Events</h2>}
+
+			{events.map((evt) => (
+				<EventItem key={evt.id} evt={evt} />
+			))}
 		</Layout>
 	)
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`)
+
+	const events = await res.json()
+
+	// this will log in the server side.
+	// console.log(events)
+
+	return {
+		props: { events },
+		revalidate: 1,
+	}
 }
